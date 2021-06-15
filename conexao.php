1e693dcd6ecfbd80e7c2b00e7b42ce624a3226bb
Rodrigo Lima <47004048+Rodrigolsantoswk@users.noreply.github.com>
2021-06-15 11:10:07 -0300

    Add files via upload

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-define('HOST', 'sql300.rf.gd');
-define('USUARIO', 'epiz_26021611');
-define('SENHA', 'xDdC6Uu25fcrh');
-define('DB', 'epiz_26021611_rhcLotas');
+define('HOST', '127.0.0.1');
+define('USUARIO', 'root');
+define('SENHA', '');
+define('DB', 'rhclotas');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('ERRO AO CONECTAR'.mysqli_error($conexao));
 ?>
